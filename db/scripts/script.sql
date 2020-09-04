7fd091750b180fc5b34a09bf6fd574fc6f941c4d
KrylovDenisK <d.krylovk@gmail.com>
2020-09-04 16:57:27 +0700

    db script

--- db/scripts/script.sql
+++ db/scripts/script.sql
@@ -1,19 +1,19 @@
 create table if not exists halls (
       id serial,
-      row integer,
-      place integer,
-      price integer,
+      row integer not null,
+      place integer not null,
+      price integer not null,
       status boolean default true,
       primary key (id)
     );
 
 create table if not exists accounts (
 	id serial,
-	fio varchar(100),
-	phone varchar(100),
-	idHall integer,
+	fio varchar(100) not null,
+	phone varchar(100) unique not null,
+	idHall integer not null,
 	primary key (id),
-	foreign key (idHall) references halls (id)
+	foreign key (idHall) references halls (id) on cascade
 );
 
 insert into halls (row, place, price) values (1, 2, 500),

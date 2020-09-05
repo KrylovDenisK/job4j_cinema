9515cfab8b96aaf9d61e2a4913f5d0d0237731bb
KrylovDenisK <d.krylovk@gmail.com>
2020-09-05 11:33:54 +0700

    db script

--- db/scripts/script.sql
+++ db/scripts/script.sql
@@ -1,22 +1,23 @@
-create table if not exists halls (
-      id serial,
-      row integer not null,
-      place integer not null,
-      price integer not null,
-      status boolean default true,
-      primary key (id)
+--Ряд - место
+CREATE TABLE IF NOT EXISTS halls (
+      id      SERIAL PRIMARY KEY,
+      row     INTEGER NOT NULL,
+      place   INTEGER NOT NULL,
+      price   INTEGER NOT NULL,
+      status  BOOLEAN DEFAULT TRUE,
     );
 
-create table if not exists accounts (
-	id serial,
-	fio varchar(100) not null,
-	phone varchar(100) unique not null,
-	idHall integer not null,
-	primary key (id),
-	foreign key (idHall) references halls (id) on cascade
+--Пользователи
+CREATE TABLE IF NOT EXISTS accounts (
+	id      SERIAL PRIMARY KEY,
+	fio     VARCHAR (100) NOT NULL,
+	phone   VARCHAR (100) UNIQUE NOT NULL,
+	idHall  INTEGER NOT NULL,
+	FOREIGN KEY(idHall) REFERENCES halls (id) ON CASCADE
 );
 
-insert into halls (row, place, price) values (1, 2, 500),
+--Загрузка мест и цен
+INSERT INTO halls (row, place, price) VALUES (1, 2, 500),
                                                  (1, 2, 500),
                                                  (1, 3, 500),
                                                  (2, 1, 1000),
